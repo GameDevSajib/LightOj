5be227f9c535d686d5f27f8bf05b9646cc988c0b
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-04-08 04:28:40 -0700

    Add files via upload

---
+++ Box1.sh
@@ -0,0 +1,21 @@
+###BISMILLAH
+read t
+while((t--))
+do
+     read n
+     for((i=0;i<n;i++))
+     do
+         for((j=0;j<n;j++))
+          do
+                 printf "*"
+          done
+          
+           echo
+     done
+
+       if((t));
+        then
+             echo
+       fi
+
+done
